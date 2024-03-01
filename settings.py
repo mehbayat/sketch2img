33553f6babb768bb9f51cdaa6fce23a550998866
Code Crafters Corner <SharCoding@gmail.com>
2024-03-01 10:57:35 +0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,5 @@
+OUTPUT_PATH = "D:/AI/ComfyUI_windows_testing/ComfyUI/output"
+INPUT_PATH = "D:/AI/ComfyUI_windows_testing/ComfyUI/input"
+URL = "http://127.0.0.1:8188/prompt"
+WORKFLOW_FILE = "workflows/sketch2image_api.json"
+MISSING_SKETCH = "media/missing-sketch.jpg"
